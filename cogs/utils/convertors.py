becebe045e4571b4bc847210ee2c7e8bbe8dd0c1
fuzzysearch404 <roberts.ziedins@inbox.lv>
2021-05-09 00:13:25 +0300

    Suppress HttpException in get/fetch methods

--- cogs/utils/convertors.py
+++ cogs/utils/convertors.py
@@ -1,15 +1,20 @@
+from discord.errors import HTTPException
+from contextlib import suppress
 
 
 async def get_fetch_member(guild, user_id):
     member = guild.get_member(user_id)
     if not member:
-        member = await guild.fetch_member(user_id)
+        with suppress(HTTPException):
+            member = await guild.fetch_member(user_id)
 
     return member
+
 
 async def get_fetch_guild(bot, guild_id):
     guild = bot.get_guild(guild_id)
     if not guild:
-        guild = await bot.fetch_guild(guild_id)
+        with suppress(HTTPException):
+            guild = await bot.fetch_guild(guild_id)
 
     return guild
